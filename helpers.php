662505e9714ee2418b7970e03e04da91e85bb27e
Burhan Ibrahimi <burhanibrahimi@orcaya.com>
2023-07-29 15:07:11 +0200

    Bismillah

---
+++ helpers.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Str;
+
+if (!function_exists('my_snake_to_title')) {
+    /**
+     * @param $value
+     *
+     * @return string
+     */
+    function my_snake_to_title($value)
+    {
+        return Str::title(str_replace('_', ' ', $value));
+    }
+    ;
+}
+
+if (!function_exists('my_camel_to_title')) {
+    /**
+     * @param $camelStr
+     *
+     * @return string|string[]|null
+     */
+    function my_camel_to_title($camelStr)
+    {
+        $intermediate = preg_replace('/(?!^)([[:upper:]][[:lower:]]+)/', ' $0', $camelStr);
+        return preg_replace('/(?!^)([[:lower:]])([[:upper:]])/', '$1 $2', $intermediate);
+    }
+}
+
+if (!function_exists('my_date_format')) {
+    function my_date_format($date = null, string $format = 'Y-m-d H:i:s'): string
+    {
+        return optional($date)->format($format) ?? "";
+    }
+}
+
+if (!function_exists('my_format_price')) {
+    /**
+     * @param        $value
+     * @param string $currency
+     * @param string $space
+     *
+     * @return string
+     */
+    function my_format_price($value, $currency = '€', $space = ' ')
+    {
+        return trim(html_entity_decode($currency, ENT_COMPAT, 'UTF-8') . $space . number_format($value, 2, ',', '.'));
+    }
+}
+
---
+++ readme.md
@@ -0,0 +1,15 @@
+1. laravel new projectname
+
+2. copy packages directory
+
+3. add requirements to main composer
+
+4. add helper file to composer autoload files
+
+5. run composer install
+
+6. register AdminServiceProvider
+
+7. run admin:fresh
+
+8. run admin:user
