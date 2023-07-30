c48c05cd688f27eb92bafa8b9a5dace2bdd5e604
Burhan Ibrahimi <burhanibrahimi@orcaya.com>
2023-07-30 11:37:29 +0200

    Update to arrayable trait for backend enums

--- src/Traits/Enum/ArrayableEnumTrait.php
+++ src/Traits/Enum/ArrayableEnumTrait.php
@@ -6,6 +6,6 @@ trait ArrayableEnumTrait
 {
     public static function toArray()
     {
-        return array_map(fn(self $item) => $item->name, self::cases());
+        return array_map(fn(self $item) => isset($item->value) ? $item->value : $item->name, self::cases());
     }
 }
