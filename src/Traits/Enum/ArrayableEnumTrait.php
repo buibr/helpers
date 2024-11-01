f1dcc54be796aac2f902f099422a43efaa70d665
Burhan Ibrahimi <burhanibrahimi@orcaya.com>
2024-11-01 06:44:51 +0100

    update arrayable with associative possibility

--- src/Traits/Enum/ArrayableEnumTrait.php
+++ src/Traits/Enum/ArrayableEnumTrait.php
@@ -7,7 +7,7 @@ trait ArrayableEnumTrait
     public static function toArray(bool $assoc = false)
     {
         if($assoc){
-            $keys = array_map(fn(self $item) => isset($item->value) ? $item->value : $item->name, self::cases());
+            $keys = array_map(fn(self $item) => $item->name, self::cases());
             $values = array_map(fn(self $item) => isset($item->value) ? $item->value : $item->name, self::cases());
             return array_combine($keys, $values);
         }
